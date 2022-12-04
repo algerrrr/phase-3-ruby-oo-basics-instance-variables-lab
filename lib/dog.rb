280eb14f6bcd8a9d4c14730daa92e802bd5d693c
Algernon Soriano <alger.soriano@gmail.com>
2022-12-04 19:22:17 +1000

    dog2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    def name
+        @this_dogs_name
+    end
 end
+
+# class Dog
+#     attr_accessor :color
+# end
+
+# dog1 = Dog.new()
+# dog1.color = "blue"
+
+# puts dog1.color
